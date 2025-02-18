b0715cb355a55991061af939bae38b0504250ed5
Sreerag Krishan <sreeragkb18@gmail.com>
2025-02-18 07:58:15 +0530

    18/02/2025

--- server/Controller/userController.js
+++ server/Controller/userController.js
@@ -1,6 +1,7 @@
 const user=require('../Model/userSchema')
 const bcrypt = require('bcrypt');
 const argon2 = require('argon2');
+const router = require('../Router/userRouter');
 const Signup=async(req,res)=>{
 try{
     console.log("postman data",req.body);
@@ -13,6 +14,63 @@ return res.status(500).json(err)
 }
 }
 
+const getDatafromDatabase=async(req,res)=>{
+    try{
+const a=await user.find()
+return res.status(200).json(a)
+    }catch(err){
+return res.status(500).json(err)
+    }
+}
+
+const getSingleDataFromdatabase=async(req,res)=>{
+    
+    
+    try{
+const a=await user.findById(req.params.userId)
+return res.status(200).json(a)
+    }catch(err){
+return res.status(500).json(err)
+    }
+}
+
+const getSingledatafromqueryindatabase=async(req,res)=>{
+    console.log(req.query);
+    
+    try{
+const a=await user.findOne({email:req.query.gmail})
+return res.status(200).json(a)
+    }catch(err){
+return res.status(500).json(err)
+    }
+}
+
+const deleteDatafromDatabase=async(req,res)=>{
+    try{
+await user.findByIdAndDelete(req.params.Id)
+return res.status(200).json("deleted successfully")
+    }catch(err){
+return res.status(500).json(err)
+    }
+}
+
+const deletedatabasefromquery=async(req,res)=>{
+    try{
+await user.findOneAndDelete({mobile:req.query.phone})
+return res.status(200).json("deleteData from databse successfully")
+    }catch(err){
+return res.status(500).json(err)
+    }
+}
+
+const updatedDataFromDatabase=async(req,res)=>{
+    try{
+const a=await user.findByIdAndUpdate(req.params.id,{$set:req.body},{new:true})
+return res.status(200).json(a)
+    }catch(err){
+return res.status(500).json(err)
+    }
+}
 
 
-module.exports={Signup}
+module.exports={Signup,getDatafromDatabase,getSingleDataFromdatabase,getSingledatafromqueryindatabase,deleteDatafromDatabase,deletedatabasefromquery,updatedDataFromDatabase}
--- server/Router/userRouter.js
+++ server/Router/userRouter.js
@@ -1,6 +1,11 @@
 const router=require('express').Router()
-const {Signup}=require('../Controller/userController')
+const {Signup,getDatafromDatabase, getSingleDataFromdatabase, getSingledatafromqueryindatabase, deleteDatafromDatabase, deletedatabasefromquery, updatedDataFromDatabase}=require('../Controller/userController')
 
 router.post('/postdata',Signup)
-
+router.get('/getData',getDatafromDatabase)
+router.get('/getSingleData/:userId',getSingleDataFromdatabase)
+router.get('/getSingleDatafromquery',getSingledatafromqueryindatabase)
+router.delete('/deleteDatabase/:Id',deleteDatafromDatabase)   //http://localhost:3000/api/deleteDatabase/67aea563a94886fffda464ef
+router.delete('/deleteDatafromdatabasefromquery',deletedatabasefromquery) //http://localhost:3000/api/deleteDatafromdatabasefromquery?phone=854693210
+router.put('/updataDatabase/:id',updatedDataFromDatabase)  //http://localhost:3000/api/updataDatabase/67ad58d5731f032cfd8d563e
 module.exports=router
