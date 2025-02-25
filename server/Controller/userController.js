e67c8945efdad32b2b8395b9dda5b5c9d2ab651a
Sreerag Krishan <sreeragkb18@gmail.com>
2025-02-25 20:31:54 +0530

    25/02/2025

--- server/Controller/userController.js
+++ server/Controller/userController.js
@@ -1,18 +1,21 @@
 const user=require('../Model/userSchema')
-const bcrypt = require('bcrypt');
 const argon2 = require('argon2');
-const router = require('../Router/userRouter');
 const Signup=async(req,res)=>{
+
+    const {username,email,mobile,password}=req.body
 try{
+    if(!username || !email || !mobile || !password){
+        return res.status(401).json("must add all items")
+    }
     console.log("third check",req.body);
     // req.body.password=await bcrypt.hash(req.body.password, 10)
     req.body.password = await argon2.hash(req.body.password);
 const databaseData=await user.create(req.body)
-return res.status(200).json({type:"signup suceess",ok:databaseData})
+return res.status(200).json({type:"signup suceess",ok:databaseData}) 
 }catch(err){
 return res.status(500).json(err)
 }
-}
+} 
 
 const getDatafromDatabase=async(req,res)=>{
     try{
@@ -24,11 +27,11 @@ return res.status(500).json(err)
 }
 
 const getSingleDataFromdatabase=async(req,res)=>{
-    
+    console.log(req.params.userId);
     
     try{
 const a=await user.findById(req.params.userId)
-return res.status(200).json(a)
+return res.status(200).json({data:a})
     }catch(err){
 return res.status(500).json(err)
     }
@@ -38,7 +41,7 @@ const getSingledatafromqueryindatabase=async(req,res)=>{
     console.log(req.query);
     
     try{
-const a=await user.findOne({email:req.query.gmail})
+const a=await user.findOne({email:req.query.xyz})
 return res.status(200).json(a)
     }catch(err){
 return res.status(500).json(err)
@@ -55,6 +58,8 @@ return res.status(500).json(err)
 }
 
 const deletedatabasefromquery=async(req,res)=>{
+    console.log("frendent values ..............",req.query.phone);
+    
     try{
 await user.findOneAndDelete({mobile:req.query.phone})
 return res.status(200).json("deleteData from databse successfully")
@@ -65,7 +70,20 @@ return res.status(500).json(err)
 
 const updatedDataFromDatabase=async(req,res)=>{
     try{
+
 const a=await user.findByIdAndUpdate(req.params.id,{$set:req.body},{new:true})
+if(!a){
+    return res.status(401).json("user is not found")
+}
+return res.status(200).json(a)
+    }catch(err){
+return res.status(500).json(err)
+    }
+}
+
+const updatedatasinquery=async(req,res)=>{
+    try{
+const a=await user.findOneAndUpdate({email:req.query.gmail},{$set:req.body},{new:true})
 return res.status(200).json(a)
     }catch(err){
 return res.status(500).json(err)
@@ -73,4 +91,4 @@ return res.status(500).json(err)
 }
 
 
-module.exports={Signup,getDatafromDatabase,getSingleDataFromdatabase,getSingledatafromqueryindatabase,deleteDatafromDatabase,deletedatabasefromquery,updatedDataFromDatabase}
+module.exports={Signup,getDatafromDatabase,getSingleDataFromdatabase,getSingledatafromqueryindatabase,deleteDatafromDatabase,deletedatabasefromquery,updatedDataFromDatabase,updatedatasinquery}
--- server/Router/userRouter.js
+++ server/Router/userRouter.js
@@ -1,5 +1,5 @@
 const router=require('express').Router()
-const {Signup,getDatafromDatabase, getSingleDataFromdatabase, getSingledatafromqueryindatabase, deleteDatafromDatabase, deletedatabasefromquery, updatedDataFromDatabase}=require('../Controller/userController')
+const {Signup,getDatafromDatabase, getSingleDataFromdatabase, getSingledatafromqueryindatabase, deleteDatafromDatabase, deletedatabasefromquery, updatedDataFromDatabase, updatedatasinquery}=require('../Controller/userController')
 
 router.post('/postdata',Signup)
 router.get('/getData',getDatafromDatabase)
@@ -8,4 +8,5 @@ router.get('/getSingleDatafromquery',getSingledatafromqueryindatabase)
 router.delete('/deleteDatabase/:Id',deleteDatafromDatabase)   //http://localhost:3000/api/deleteDatabase/67aea563a94886fffda464ef
 router.delete('/deleteDatafromdatabasefromquery',deletedatabasefromquery) //http://localhost:3000/api/deleteDatafromdatabasefromquery?phone=854693210
 router.put('/updataDatabase/:id',updatedDataFromDatabase)  //http://localhost:3000/api/updataDatabase/67ad58d5731f032cfd8d563e
+router.put('/updatedatainquery',updatedatasinquery)
 module.exports=router
